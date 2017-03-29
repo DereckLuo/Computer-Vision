7a310230c8d71c1dbd289e30627a51c8b81f46da
Dereck <dereckluo@yahoo.com>
2017-03-29 13:56:28 -0500

    add hw4

---
+++ Homework4/hw4_supp/p1/slic.m
@@ -0,0 +1,22 @@
+function [cIndMap, time, imgVis] = slic(img, K, compactness)
+
+%% Implementation of Simple Linear Iterative Clustering (SLIC)
+%
+% Input:
+%   - img: input color image
+%   - K:   number of clusters
+%   - compactness: the weighting for compactness
+% Output: 
+%   - cIndMap: a map of type uint16 storing the cluster memberships
+%   - time:    the time required for the computation
+%   - imgVis:  the input image overlaid with the segmentation
+
+tic;
+% Put your SLIC implementation here
+
+
+% 
+time = toc;
+
+end
+
